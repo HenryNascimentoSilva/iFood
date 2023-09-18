b30abd305b11fd43b26fa579f2365300f61083b4
HenryNascimentoSilva <henrynascimentosilva@gmail.com>
2023-09-18 01:21:23 -0300

    Modelos

--- src/main/java/me/dio/sacola/model/Client.java
+++ src/main/java/me/dio/sacola/model/Client.java
@@ -1,4 +1,15 @@
 package me.dio.sacola.model;
 
+import jakarta.persistence.Embedded;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Client {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String username;
+  @Embedded
+  private Adress adress;
 }
