1984eeb41446ff64552a3fd6f964d65c54609b27
HenryNascimentoSilva <henrynascimentosilva@gmail.com>
2023-09-18 02:46:51 -0300

    Criando classe Cliente

--- src/main/java/me/dio/sacola/model/Client.java
+++ src/main/java/me/dio/sacola/model/Client.java
@@ -1,10 +1,18 @@
 package me.dio.sacola.model;
 
-import jakarta.persistence.Embedded;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
 public class Client {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
