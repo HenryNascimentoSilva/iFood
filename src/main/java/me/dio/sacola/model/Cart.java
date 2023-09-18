d51b37600cc41cd9580f9b48778d7b7dc24674bb
HenryNascimentoSilva <henrynascimentosilva@gmail.com>
2023-09-18 02:18:38 -0300

    Criando classe Cart

--- src/main/java/me/dio/sacola/model/Cart.java
+++ src/main/java/me/dio/sacola/model/Cart.java
@@ -6,19 +6,30 @@ import me.dio.sacola.enumeration.PaymentMethod;
 
 import java.util.List;
 
+// @ Entity representa um banco de dados
+@Entity
 public class Cart {
 
+  // primeira Key gerada automaticamente
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
+
+  // um cliente pode ter varios carts
   @ManyToOne(fetch = FetchType.LAZY, optional = false)
   @JsonIgnore
   private Client client;
+
+  // um item para vários clientes
   @OneToMany(cascade = CascadeType.ALL)
   private List<Item> itens;
+
+  // valor total dos produtos no cart
   private Double totalValue;
 
+  // método de pagamento(dinheiro ou cartão)
   @Enumerated
   private PaymentMethod payment;
+
   private boolean closed;
 }
