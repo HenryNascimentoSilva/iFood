120c5822b31a2b38a922b50b057fdfe7dbe265cf
HenryNascimentoSilva <henrynascimentosilva@gmail.com>
2023-09-19 03:24:14 -0300

    GAMBIARRA

--- src/main/java/me/dio/sacola/service/impl/CartServiceImpl.java
+++ src/main/java/me/dio/sacola/service/impl/CartServiceImpl.java
@@ -4,19 +4,54 @@ import lombok.RequiredArgsConstructor;
 import me.dio.sacola.enumeration.PaymentMethod;
 import me.dio.sacola.model.Cart;
 import me.dio.sacola.model.Item;
+import me.dio.sacola.model.Restaurant;
 import me.dio.sacola.repository.CartRepository;
+import me.dio.sacola.repository.ItemRepository;
+import me.dio.sacola.repository.ProductRepository;
 import me.dio.sacola.resource.dto.ItemDto;
 import me.dio.sacola.service.CartService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CartServiceImpl implements CartService {
 	private final CartRepository cartRepository;
+	private final ProductRepository productRepository;
+	private final ItemRepository itemRepository;
 
 	@Override
 	public Item insertItem(ItemDto itemDto) {
-		return null;
+		Cart cart = seeCart(itemDto.getIdCart());
+
+		if(cart.isClosed()){
+			throw new RuntimeException("This cart is closed!");
+		}
+
+		Item item = Item.builder()
+				.quantity(itemDto.getQuantity())
+				.cart(cart)
+				.product(productRepository.findById(itemDto.getProductId()).
+						orElseThrow(() -> new RuntimeException("This product does not exist")
+						))
+				.build();
+
+		List<Item> itens = cart.getItens();
+		if(itens.isEmpty()){
+			itens.add(item);
+		}
+		else {
+			Restaurant restaurant = itens.get(0).getProduct().getRestaurant();
+			Restaurant addItem = item.getProduct().getRestaurant();
+			if(restaurant.equals(addItem)){
+				itens.add(item);}
+				else{
+					throw new RuntimeException("This product does not belong to this restaurant!");
+				}
+			}
+		cartRepository.save(cart);
+		return itemRepository.save(item);
 	}
 
 	@Override
